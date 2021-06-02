83a1e81ea07ad5c2b7e0119f55869b131e2eb43d
Glitch (aoiki) <none>
2021-06-02 07:35:57 +0000

    🎱🏥 Checkpoint ./index.js:32100315/2049

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // load env file (contains important keys)
-require('dotenv').config();
+require("dotenv").config();
 
-const fs = require('fs');
+const fs = require("fs");
 
 const Client = require(`${process.cwd()}/struct/Client`);
 const config = require(`${process.cwd()}/config`);
@@ -12,9 +12,17 @@ const options = {
   bypass: true,
   log: true,
   paths: [
-    'action', 'anime', 'bot',
-    'core', 'fun', 'moderation', 'music',
-    'owner', 'setup', 'social','utility'
+    "action",
+    "anime",
+    "bot",
+    "core",
+    "fun",
+    "moderation",
+    "music",
+    "owner",
+    "setup",
+    "social",
+    "utility"
   ]
 };
 
@@ -22,18 +30,17 @@ client.database?.init();
 
 client.musicPlayer?.init();
 
-client.loadCommands({ parent: 'commands', ...options });
+client.loadCommands({ parent: "commands", ...options });
 
-client.loadEvents({ parent: 'events', ...options });
+client.loadEvents({ parent: "events", ...options });
 
-client.defineCollections([ 'discovery', 'economy', 'memes', 'xp' ]);
+client.defineCollections(["discovery", "economy", "memes", "xp"]);
 
 // let client listen to process events, setting ignore to true will
 // ignore errors and not execute the functions from util/processEvents.js.
 // Available process events on https://nodejs.org/api/process.html#process_process_events
-client.listentoProcessEvents([
-  'unhandledRejection',
-  'uncaughtException'
-], { ignore: false });
+client.listentoProcessEvents(["unhandledRejection", "uncaughtException"], {
+  ignore: false
+});
 
 client.login();
